7ea472aefc63f511ea7fe5d515f900e1498fa43b
vishu pratap <vishurizz0@gmail.com>
2024-11-07 10:16:19 +0530

    les goo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div class="container py-4 px-3 mx-auto">
+    <h1>this is going to be that crazzy multiverse of crazy ass BU spaces</h1>
+      <button class="btn btn-primary">let's fuck it</button>
+    </div>
+    </>
+  )
+}
+
+export default App
